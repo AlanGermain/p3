244552b00f0caa5b99391d503814bed62a3defbf
alan germain <alan.germain16@gmail.com>
2023-03-20 11:14:20 +0100

    modale ajout

--- Portfolio-architecte-sophie-bluel-master/FrontEnd/assets/admin.js
+++ Portfolio-architecte-sophie-bluel-master/FrontEnd/assets/admin.js
@@ -45,14 +45,7 @@ if (getToken !== null) {
 
 //\\\\\\\\\\\\\\\\\Modale d'ouverture/////////////////////\\
 
-let modal = null;
-
-// Arrêter la propagation de la modale
-function stopPropagation(e) {
-  e.stopPropagation();
-}
-
-//Fonction appelée pour faire apparaître les projets dans la modale
+// Fonction appelée pour faire apparaître les projets dans la modale
 async function showProjectsModal() {
   const response = await fetch("http://localhost:5678/api/works");
   const json = await response.json();
@@ -74,49 +67,126 @@ async function showProjectsModal() {
     // Au clic du bouton, on exécute la fonction (sur l'id qu'on pointe)
     deleteButton.setAttribute("onclick", "deleteProject(this.id);");
     deleteButton.classList.add("bouton-modale-delete");
-    const trashIcone = document.createElement("img");
-    trashIcone.src = "assets/icons/trash-can-solid.svg";
-    trashIcone.classList.add("icone-modale-delete");
+    const icon = document.createElement("span");
+    icon.innerHTML =
+      '<i class="fa-solid fa-trash-can icone-modale-delete"></i>';
+    const moveIcone = document.querySelector(".move-icone");
+    divProjects.appendChild(figureElement);
     figureElement.appendChild(imageElement);
     figureElement.appendChild(figcaptionElement);
-    deleteButton.appendChild(trashIcone);
     figureElement.appendChild(deleteButton);
-    divProjects.appendChild(figureElement);
+    figureElement.appendChild(moveIcone);
+    deleteButton.appendChild(icon);
+  });
+  // Ajouter l'icône pour déplacer les projets (déco)
+  let moveFigure = document.createElement("span");
+  moveFigure.innerHTML =
+    '<i class="fa-solid fa-arrows-up-down-left-right move-icone"></i>';
+  console.log(moveFigure);
+  let test = json[0];
+  console.log(test);
+  // test.appendChild(moveFigure);
+  // test.push(moveFigure);
+}
+
+// Arrêter la propagation de la modale
+function stopPropagation(e) {
+  e.stopPropagation();
+}
+
+// Fonction pour ouvrir la modale
+function openModal(modal, button, cross, wrapper) {
+  // Les paramètres sont utiles pour choisir quelle modale on souhaite ouvrir
+  const chooseModal = document.getElementById(modal);
+  const chooseButton = document.getElementById(button);
+  if (chooseButton !== null) {
+    chooseButton.addEventListener("click", function () {
+      chooseModal.style.display = "flex";
+    });
+    chooseModal.addEventListener("click", closeModal);
+    // Fermer la modale au clic sur la croix
+    chooseModal.querySelector(cross).addEventListener("click", closeModal);
+    // Fermer la modale au clic à l'extérieur de la modale
+    chooseModal
+      .querySelector(wrapper)
+      .addEventListener("click", stopPropagation);
+  }
+}
+
+// Fonction pour fermer la modale
+function closeModal() {
+  const chooseModal = document.getElementById("modal1");
+  chooseModal.style.display = "none";
+}
+
+// Appel des fonctions pour la modale dans une fonction globale (une pour chaque modale)
+function openFirstModal() {
+  document.querySelectorAll(".open-modal1").forEach((call) => {
+    call.addEventListener(
+      "click",
+      openModal("modal1", "open-modal-wrapper", ".icone", ".modal-wrapper")
+    );
+    showProjectsModal();
   });
 }
 
-// Pour ouvrir la modale
-const openModal = function (e) {
-  e.preventDefault();
-  const target = document.querySelector(e.target.getAttribute("href"));
-  target.style.display = null;
-  target.removeAttribute("aria-hidden");
-  target.setAttribute("aria-modal", "true");
-  modal = target;
+openFirstModal();
 
-  //Fermer la modale grâce à la croix
-  const closeIcone = document.querySelector(".icone");
-  closeIcone.addEventListener("click", closeModal);
+//\\\\\\\\\\\\\\\\\Suppression de projet/////////////////////\\
 
-  //Fermer la modale au clic à l'extérieur
-  const closeModalOutside = document.querySelector(".modal-wrapper");
-  closeModalOutside.addEventListener("click", stopPropagation);
-  document.querySelector("#modal1").addEventListener("click", closeModal);
-};
+async function deleteProject(clicked_id) {
+  const response = await fetch(
+    `http://localhost:5678/api/works/${clicked_id}`,
+    {
+      method: "DELETE",
+      headers: {
+        "Content-Type": "application/json",
+        authorization: `Bearer ${getToken}`,
+      },
+    }
+  );
 
-// Pour fermer la modale
-const closeModal = function (e) {
-  if (modal === null) return;
-  e.preventDefault();
-  modal.style.display = "none";
-  modal.setAttribute("aria-hidden", "true");
-  modal.removeAttribute("aria-modal");
-  modal.removeEventListener("click", closeModal);
-  modal = null;
-};
+  // Ne pas rafraîchir la page quand on supprime un projet
+  const deleteModalProjectsNoRefresh = document.getElementById(
+    "projets " + clicked_id
+  );
+  deleteModalProjectsNoRefresh.remove();
 
-// La fonction pour ouvrir la modale est appelée grâce à un addEventListener
-document.querySelectorAll(".open-modal1").forEach((a) => {
-  a.addEventListener("click", openModal);
-  showProjectsModal();
+  const deleteProjectsNoRefresh = document.getElementById(
+    "galery " + clicked_id
+  );
+  deleteProjectsNoRefresh.remove();
+}
+
+//\\\\\\\\\\\\\\\\\Modale d'ajout/////////////////////\\
+
+// Fonction pour choisir la modale à afficher
+function changeModal(modal1, modalDirection, modal2) {
+  document.querySelector(".modal-wrapper").style.display = modal1;
+  // Ici on change la direction de la modale, afin de lui redonner son état d'origine (qui est en colonne)
+  document.querySelector(".modal-wrapper").style.flexDirection = modalDirection;
+  document.querySelector(".modale-upload").style.display = modal2;
+}
+
+// Fonction pour ouvrir la 2e modale
+function openSecondModal() {
+  document.querySelectorAll(".open-modal-upload").forEach((call) => {
+    call.addEventListener("click", () => {
+      changeModal("none", "", "flex");
+      openModal(
+        "modal1",
+        "open-second-modal",
+        ".cross-upload",
+        ".modale-upload"
+      );
+    });
+  });
+}
+
+openSecondModal();
+
+// Sert à utiliser la flèche de la 2e modale afin de revenir en arrière
+let returnFirstModale = document.querySelector(".arrow");
+returnFirstModale.addEventListener("click", () => {
+  changeModal("flex", "column", "none");
 });
